5a59e8038a2b58732bfb3357f42f2184ef1ffcd3
Vishal-143x <Vishalpatil080502@gmail.com>
2022-11-20 18:13:04 +0530

    Total Watch count section added

--- pages/new.js
+++ pages/new.js
@@ -14,13 +14,13 @@ function New() {
             name: 'Page A',
             uv: 4000,
             pv: 2400,
-            amt: 2400,
+
         },
         {
             name: 'Page B',
             uv: 3000,
             pv: 1398,
-            amt: 2210,
+
         },
         {
             name: 'Page C',
@@ -61,19 +61,25 @@ function New() {
                 <div className="flex-1 text-gray-700  p-20 text-center mt-2 ">
 
 
-                    <div className=' grid grid-row-3 grid-flow-col w-full rounded-lg  mb-16 '>
-                        <div className=' p-3 m-6 flex-col bg-white mt-2 h-22 text-center justify-center  cursor-pointer shadow-2xl flex rounded-xl'>
+                    <div className=' grid grid-row-4 grid-flow-col w-full rounded-lg mt-18 mb-16 '>
+
+                        <div className=' p-3  m-6 flex-col bg-white mt-2 h-22 text-center justify-center cursor-pointer shadow-2xl flex rounded-xl'>
+                            <h2 className='text-xl w-full mt-2 text-center items-center justify-center '>Total Watch  Count  </h2>
+                            <h2 className='text-xl text-purple-600 hover:text-blue-300 '>1000</h2>
+                        </div>
+
+                        <div className=' p-3 m-6  flex-col bg-white mt-2 h-22 text-center justify-center cursor-pointer shadow-2xl flex rounded-xl'>
                             <h2 className='text-xl  mt-2 text-center items-center justify-center '>Total Value Locked </h2>
                             <h2 className='text-xl text-gray-600 hover:text-orange-800 '>$2000</h2>
                         </div>
 
-                        <div className=' p-3 m-6  flex-col bg-white mt-2 h-22 text-center justify-center font cursor-pointer  shadow-2xl flex rounded-xl'>
+                        <div className=' p-3 m-6  flex-col bg-white mt-2 h-22 text-center justify-center cursor-pointer shadow-2xl flex rounded-xl'>
                             <h2 className='text-xl  mt-2 text-center items-center justify-center '>Total Value  Spent </h2>
                             <h2 className='text-xl text-red-600 hover:text-orange-600'>$1000</h2>
                         </div>
 
                         <div className=' p-3 m-6  flex-col bg-white mt-2 h-22 text-center justify-center cursor-pointer shadow-2xl flex rounded-xl'>
-                            <h2 className='text-xl  mt-2 text-center items-center justify-center '>Remaining Amount </h2>
+                            <h2 className='text-xl  mt-2 text-center items-center justify-center '>Remaining Amount  </h2>
                             <h2 className='text-xl text-green-600 hover:text-green-500 '>$1000</h2>
                         </div>
 
@@ -118,18 +124,18 @@ function New() {
                             </button>
                         </Link>
 
-                        
-                            <button
-                                type="submit"
-                                className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500  uppercase  text-white py-3 mt-6 rounded-full w-3/5">
-                                Withdraw Your Funds
-                            </button>
+
+                        <button
+                            type="submit"
+                            className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500  uppercase  text-white py-3 mt-6 rounded-full w-3/5">
+                            Withdraw Your Funds
+                        </button>
 
                     </div>
                 </div>
             </div>
 
-        </div>
+        </div >
     )
 }
 
